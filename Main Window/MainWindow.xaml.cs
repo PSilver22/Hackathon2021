2e0f25560a49c3a2094ff20726ed4ad9ed60ebca
Avraham Meyers <a32100meyers@gmail.com>
2021-11-30 18:38:41 +0200

    added listbox method

--- Main Window/MainWindow.xaml.cs
+++ Main Window/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Data_Structures;
+
 
 namespace Main_Window
 {
@@ -20,9 +22,48 @@ namespace Main_Window
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<Employee> employees = new();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void AddEmployeeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Battery battery = new();
+
+            battery.Capacity = double.Parse(BatteryCapacity.Text);
+            battery.CurrentLevel = double.Parse(CurrentBattery.Text);
+
+            Car car = new();
+            car.LicensePlateNumber = int.Parse(LicensePlate.Text);
+            car.ItsBattery = battery;
+
+            Employee Employee = new();
+            Employee.Name = Name.Text;
+            Employee.ItsCar = car;
+
+            employees.Add(Employee);
+        }
+
+        private void CreateListBoxItem(string text, string buttonText, RoutedEventHandler clickEvent)
+        {
+            Button button = new();
+            button.Content = buttonText;
+            button.Click += clickEvent;
+
+            Label label = new();
+            label.Content = text;
+
+            StackPanel stackPanel = new();
+            stackPanel.Children.Add(label);
+            stackPanel.Children.Add(button);
+
+            ListBoxItem listBoxItem = new();
+            listBoxItem.Content = stackPanel;
+
+
+            
         }
     }
 }
